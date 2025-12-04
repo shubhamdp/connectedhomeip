af13f5445578855c7db1bc9420b193a6f54f7d13
Shubham Patil <shubham.patil@espressif.com>
2025-12-04 10:13:43 +0530

    patch from Xiaolong (#39504)

--- src/wifipaf/WiFiPAFEndPoint.cpp
+++ src/wifipaf/WiFiPAFEndPoint.cpp
@@ -75,6 +75,7 @@
 #define WIFIPAF_ACK_SEND_TIMEOUT_MS 2500
 #define WIFIPAF_WAIT_RES_TIMEOUT_MS 1000
 // Drop the connection if network resources remain unavailable for the period.
+// Known condition: If the remote side is awaiting an ACK packet, the wait time must not exceed PAFTP_ACK_TIMEOUT_MS.
 #define WIFIPAF_MAX_RESOURCE_BLOCK_COUNT (PAFTP_CONN_IDLE_TIMEOUT_MS / WIFIPAF_WAIT_RES_TIMEOUT_MS)
 
 /**
